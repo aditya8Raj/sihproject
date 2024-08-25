1c73e84e921f1d321404d97a6198c27c485c8818
Aditya Raj <aditya7raj1@gmail.com>
2024-08-26 00:59:45 +0530

    custom color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,6 +53,7 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        heroBg: "#faf2ef",
       },
       borderRadius: {
         lg: "var(--radius)",
