24aebe2edca7f530f6a70c1b3b513af44fae2f84
Aditya Raj <aditya7raj1@gmail.com>
2024-08-26 01:02:26 +0530

    navbar and hero section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Navbar from "./components/ui/Navbar";
+import HeroSection from "./components/ui/HeroSection";
 
 function App() {
   return (
     <>
-      <div>
+      <div className="bg-slate-900">
         <Navbar />
+        <HeroSection />
       </div>
     </>
   );
 }
 
 export default App;
+// -------------------------------
