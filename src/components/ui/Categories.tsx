eab8341f6558ce0b5f913c824aabbfb00177f476
Aditya Raj <aditya7raj1@gmail.com>
2024-08-27 15:58:30 +0530

    categories added

--- src/components/ui/Categories.tsx
+++ src/components/ui/Categories.tsx
@@ -1,6 +1,6 @@
 const Categories = () => {
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div className="relative z-20 px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="grid row-gap-8 sm:row-gap-0 sm:grid-cols-2 lg:grid-cols-3">
         <div className="p-8 border-b sm:border-r">
           <div className="max-w-md text-center">
