49257814a54d24b82c20ba10e428ed0322246b3e
Aditya Raj <aditya7raj1@gmail.com>
2024-09-08 00:01:23 +0530

    tutoring academic support page made

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import SigninPage from "./pages/SigninPage";
 import RegisterPage from "./pages/RegisterPage";
+import TutoringAcademicSupportPage from "./pages/TutoringAcademicSupportPage";
 
 function App() {
   return (
@@ -13,6 +14,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SigninPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/tutoring-academic-support"
+            element={<TutoringAcademicSupportPage />}
+          />
         </Routes>
       </BrowserRouter>
     </>
